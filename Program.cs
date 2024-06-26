1db11cf831e0299d4a0084e96a9ceb8c4711382f
Alireza Khoshnami <124736009+arkh99@users.noreply.github.com>
2024-06-26 15:47:49 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int solution2(List<int> a1, List<int> a2)
+            {
+                a1.Reverse();
+                a2.Reverse();
+
+            }
+        }
+    }
+}
