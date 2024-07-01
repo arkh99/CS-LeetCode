13f9db8cbaef051124d97e1e61243e7fa1a30ab3
alireza <arkh919@gmail.com>
2024-06-30 20:49:45 -0400

    file organize

---
+++ solution_substrings/substrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace substrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int substring() {
+                
+            }
+        }
+    }
+}
